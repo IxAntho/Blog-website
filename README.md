bf9ca7ec2feadc7e2491f8a10067ce453e41ca9a
IxAntho <isabar735@icloud.com>
2024-03-18 12:03:59 -0400

    Refactor posts background and README file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Blog Website
+
+This is a simple blog website built using Flask, a Python web framework. The website fetches blog posts from a JSON API
+and displays them on the homepage. Users can click on individual posts to read the full content. The website also
+includes an About and Contact page.
+
+## Features
+
+- Display a list of blog posts on the homepage
+- Click on individual posts to view the full content
+- About and Contact pages
+
+## Prerequisites
+
+- Python 3.x
+- Flask
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 BLOG_URL = "https://api.npoint.io/c790b4d5cab58020d391"
 
+POST_BACKGROUND = ["https://images.unsplash.com/photo-1516481605912-d34c1411504c?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNhY3R1c3xlbnwwfHwwfHx8MA%3D%3D", "https://images.unsplash.com/photo-1449358070958-884ac9579399?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fGFjdGl2aXR5fGVufDB8fDB8fHww", "https://images.unsplash.com/photo-1444459094717-a39f1e3e0903?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI4fHxudXRyaXRpb258ZW58MHx8MHx8fDA%3D"]
 
 @app.route("/")
 def home_page():
@@ -32,7 +33,8 @@ def post(post_id):
     response = requests.get(url=BLOG_URL)
     all_posts = response.json()
     content = all_posts[post_id-1]
-    return render_template("post.html", post=content)
+    bg = POST_BACKGROUND[post_id-1]
+    return render_template("post.html", post=content, background=bg)
 
 
 if __name__ == "__main__":
